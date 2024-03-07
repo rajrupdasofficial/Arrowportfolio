41fd340d155a4069ea57d3c82246f5a55ed9d52d
RajrupDasid <rajrupofficialid@protonmail.com>
2024-03-07 11:02:03 +0530

    primary name fixing enabled
    Signed-off-by: RajrupDasid <rajrupofficialid@protonmail.com>

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -76,9 +76,9 @@ const NavBar = () => {
         <Link
           href="/"
           className="text-sm bg-black rounded-md p-1 font-semibold flex items-center justify-center">
-          <span className="text-white mr-1">Arrowfig</span>
+          <span className="text-white mr-1">webstackpros</span>
           <span className="w-12 h-8 rounded bg-white text-black flex items-center justify-center">
-            .com
+            .net
           </span>
         </Link>
       </div>
